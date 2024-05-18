ce303f0992c02acae47cf62ad2f80c98cd91c1c0
Messnaldo <6422781276@g.siit.tu.ac.th>
2024-05-18 13:04:03 +0700

    pip install customtkinter before

---
+++ ShowEncryptedPassword.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import customtkinter
+from tkinter import scrolledtext
+import SendEmail
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("dark-blue")
+app = customtkinter.CTk(fg_color='grey')
+app.geometry("600x440")
+app.title('Show Encrypted Password')
+AuthenMsg = tk.Label(master=app, text="Encrypted Password:.....", font=('Century Gothic', 45))
+AuthenMsg.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+app.mainloop()
