f464c2e9785cd6dfe731e60d59d96351788340d4
nopeisawesome <nopeisawesome@gmail.com>
2024-05-19 13:50:11 +0700

    Bug 'fixed'

--- ChooseYourClient.py
+++ ChooseYourClient.py
@@ -7,61 +7,72 @@ import encryptor,decryptor,symmetrickeyencrypt
 import random as rd
 from datetime import datetime
 
-Client = ''
-frames = {}
-myE,myD,myN = 0,0,0
-keys = []
-sE, sN, sD = 0,0,0
+#For code simplicity, these are the session variables
+Client = '' #The client computer that the user is using (NOT EMAIL ACCOUNT!)
+frames = {} #Dictionary for changing frames (We have two methods to change frames since we divided the frontend between two people)
+myE,myD,myN = 0,0,0  #Public, Private, N, of the Client
+keys = [] #The public keys of everyone 
+sE, sN, sD = 0,0,0 #Public, Private, N of Server. The server does not have access to Client's private key and vice versa
+
+#Changing Frames(Page)
 def changeFrame(frameName):
     frame = frames[frameName]
     frame.tkraise()
+
+#Initializing: obtaining and assigning the keys to everyone
 def getKey(client):
     global myE,myD,myN,sE,sN,keys,Client
     Client = client
     path = f"{client}keys.txt"
     with open(path,'r') as file:
-        myE,myD,myN = map(int, file.readline().strip().split(','))
+        myE,myD,myN = map(int, file.readline().strip().split(',')) #Client Keys
     with open('publicKeys.txt','r') as file:
-        keys = file.readlines()
+        keys = file.readlines()        #Public Keys
         for i in keys:
             i = i.strip().split(',')
-            if i[0] == 'S':
+            if i[0] == 'S':            #Obtaining server keys used only for client
                 sE = int(i[1])
                 sN = int(i[2])
+
+#When user chose the client to access
 def clientClick(frameName,client):
     getKey(client)
-    keytext = f"e = {myE} d = {myD} n = {myN}"
-    keyText.configure(text=keytext)
     changeFrame(frameName)
     
+#Client authenticating to the Server, this is done through a button since we do not have to switch between two python scripts
 def authenticate(d,n,skey,sn,frameName):
     global payLoad
-    # print(Client)
+    #Hashing a nonce for MAC
     shaObject = hashlib.sha256()
     nonce = f'{rd.randint(1,128)}'
     shaObject.update(nonce.encode('utf-8'))
+    #Creating the MAC
     macText = shaObject.hexdigest()
     macText = encryptor.encrypt(d,n,macText) #We are signing mac with client signature
-    authChunk = f"{nonce},{macText}"
-    authChunk = encryptor.encrypt(skey,sn,authChunk)
-    payLoad = f"{Client},{authChunk}"
-    # print(payLoad)
-    # authText.configure(text=payLoad)
+    #Making the payload for authentication 
+    authChunk = f"{nonce},{macText}" # m,{sha256(m)}_prClient
+    authChunk = encryptor.encrypt(skey,sn,authChunk) # {m,{sha256(m)}_prClient}_puServer This is to make sure that only server has access
+    payLoad = f"{Client},{authChunk}" #Adding Client to let server know which key to use
     changeFrame(frameName)
+
+#Server authenticate from the previous function ***THIS IS THE SERVER'S PERSPECTIVE***
 def serverAuth(payload):
     global sE, sD, sN
+    #Server logs for each payload that comes into the server
     with open('authentication.txt','a') as file:
         file.write(f'Client: {payload}')
         
+    #Obtaining the keys for the server
     with open('Skeys.txt','r') as file:
         sE, sD, sN = map(int, file.readline().strip().split(','))
     payload = payload.split(',')
     client = payload[0]
     macChunk = payload[1]
-    macChunk = decryptor.decrypt(sD,sN,macChunk)
+    macChunk = decryptor.decrypt(sD,sN,macChunk)    #Decrypting the payload along with obtaining the MAC
     with open('authentication.txt','a') as file:
-        file.write(f'Client Decrypted: {macChunk}')
+        file.write(f'Client Decrypted: {macChunk}') #Writing what the server decrypted into the logs
         
+    #Obtaining the m and digital signature MAC
     macChunk = macChunk.split(',')
     for i in keys:
         i = i.strip().split(',')
@@ -73,12 +84,115 @@ def serverAuth(payload):
     shaObject = hashlib.sha256()
     shaObject.update(nonce.encode('utf-8'))
     nonceHash = shaObject.hexdigest()
+    #If the mac that we obtainined matches the hashed version of the m, then we grant access to the server
     if mac == nonceHash:
         changeFrame('login')
+    #Add what the server obtained into the logs
     with open('authentication.txt','a') as file:
         file.write(f'Server Decrypted: {nonceHash}')
         file.write(f'Received MAC: {mac}')
-   
+
+#Redirecting user to sending email page
+def SendEmail():
+    frame2.place_forget()
+    login.place_forget()
+    showAuthentication.place_forget()
+    serverFrame.place_forget()
+    LoginText.place_forget()
+    frame.place_forget()
+    SendMailframe.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+#Going back to the main page
+def back_to_main_frame():
+    SendMailframe.place_forget()
+    ViewEmailframe.place_forget()
+    frame2.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+#Viewing the emails that was sent
+def ViewEmail():
+    frame2.place_forget()
+    frame2.place_forget()
+    login.place_forget()
+    showAuthentication.place_forget()
+    serverFrame.place_forget()
+    LoginText.place_forget()
+    frame.place_forget()
+    getMail()
+    ViewEmailframe.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+#Sending the mails
+def sendMail():
+    #Obtaining the destination and the body from the text box
+    to = Toentry.get()
+    body = entry44.get("1.0", tk.END).strip()
+    #Generating random number to use as synchronous key
+    sssk = rd.randint(1,100000000)
+    ssskByte = sssk.to_bytes(16, byteorder='big')
+    ssskString = f"{sssk}"
+
+    #Obtaining the public key of the receiver
+    for i in keys:
+        i = i.strip().split(',')
+        if i[0] == f'{to}':
+            receiverE = int(i[1])
+            receiverN = int(i[2])
+
+    #hashing the body as per PGP requirements
+    shaOb = hashlib.sha256()
+    shaOb.update(body.encode('utf-8'))
+    digiSig = shaOb.hexdigest()
+    digiSig = encryptor.encrypt(sD,sN,digiSig) #Digital signature
+
+    bodyEncrypted = symmetrickeyencrypt.encrypt_message(body,ssskByte) #Encrypted body message
+
+    sssKEncrypted = encryptor.encrypt(receiverE,receiverN,ssskString) #Encrypted session key for the receiver to decrypt
+
+    #Sending to one of the client's computer We are using a mailbox file to act like an email database
+    if to in ['A','B','C']:
+        with open(f'{to}_mailbox.txt','a') as file:
+            file.write(f'{Client},{sssKEncrypted},{digiSig},{bodyEncrypted},{datetime.now().strftime("%Y-%m-%d")}\n')
+    SendMailframe.place_forget()
+    ViewEmailframe.place_forget()
+    frame2.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+#Obtaining the mails
+mails = []
+def getMail():
+    global mails
+    with open(f'{Client}_mailbox.txt','r') as file: #Checking all of the emails in the file
+        for i in file.readlines():
+            i = i.strip().split(',')
+            #Decryption 
+            sender, sssKEncrypted, digiSig, bodyEncrypted, time = i
+            sssk = decryptor.decrypt(myD,myN,sssKEncrypted)
+            body = symmetrickeyencrypt.decrypt_message(bodyEncrypted,int(sssk).to_bytes(16, byteorder='big'))
+
+            #Decrypting the digital signature with the server's keys to obtain the hashed body for authentication
+            digiSig = decryptor.decrypt(sE,sN,digiSig)
+            shaOb = hashlib.sha256()
+            shaOb.update(body.encode('utf-8')) #Hashing the body obtained from the message
+            comparator = shaOb.hexdigest()
+            #***THERE IS APPARENTLY A BUG WITH THE WAY AES HASH AND PADDINGS INTERACT, WE ARE COMPARING THE FIRST 63 LETTERS 
+            if digiSig[:-1] == comparator[:-1]: #If the hashed body matches the digital signature, then show it in the mailbox
+                print('pass')
+                mails.append(f"{sender}\n{body}\n{time}\n----------\n")
+
+#Showing the mails from the previous functions in the interface
+def showMails():
+    frame2.place_forget()
+    login.place_forget()
+    showAuthentication.place_forget()
+    serverFrame.place_forget()
+    LoginText.place_forget()
+    frame.place_forget()
+    getMail()
+    text_box.config(state=tk.NORMAL)  
+    text_box.delete(1.0, tk.END)
+    for mail in mails:
+        text_box.insert(tk.END, mail)
+    text_box.config(state=tk.DISABLED) 
+    ViewEmailframe.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
         
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("dark-blue")
@@ -94,12 +208,8 @@ LoginText = customtkinter.CTkLabel(master=frame, text="Email Simulator", font=('
 LoginText.place(relx=0.5, y=45, anchor=tk.CENTER)
 EmailLabel = customtkinter.CTkLabel(master=frame, text="Please choose your client", font=('Century Gothic', 20))
 EmailLabel.place(relx=0.5, y=85, anchor=tk.CENTER)
-#Loginbutton = customtkinter.CTkButton(master=frame, width=75, height=75, text="Login", corner_radius=6)
-#Loginbutton.place(relx=0.35, y=175, anchor=tk.CENTER)
 Abutton = tk.Button(master=frame, width=6, height=1, text="A", font=("Century Gothic", 20), bg="#0097B2",fg="white",command=lambda: clientClick('authentication', "A"))
 Abutton.place(relx=0.2, y=175, anchor=tk.CENTER)
-#Signupbutton = customtkinter.CTkButton(master=frame, width=75, height=75, text="Signup", corner_radius=6)
-#Signupbutton.place(relx=0.65, y=175, anchor=tk.CENTER)
 Bbutton = tk.Button(master=frame, width=6, height=1, text="B", font=("Century Gothic", 20), bg="#FF5757",fg="white",command=lambda: clientClick('authentication', "B"))
 Bbutton.place(relx=0.5, y=175, anchor=tk.CENTER)
 Cbutton = tk.Button(master=frame, width=6, height=1, text="C", font=("Century Gothic", 20), bg="#FF914D",fg="white",command=lambda: clientClick('authentication', "C"))
@@ -114,15 +224,6 @@ LoginText = customtkinter.CTkLabel(master=serverFrame, text="Connect to server",
 LoginText.place(relx=0.5, y=75, anchor=tk.CENTER)
 Abutton = tk.Button(master=serverFrame, width=6, height=1, text="A", font=("Century Gothic", 20), bg="#0097B2",fg="white",command=lambda: authenticate(myD,myN,sE,sN,'showAuth'))
 Abutton.place(relx=0.2, y=175, anchor=tk.CENTER)
-keyText = customtkinter.CTkLabel(master=serverFrame, text=f"e = {myE} d = {myD} n = {myN}", font=('Century Gothic', 40))
-keyText.place(relx=0.5, y=225, anchor=tk.CENTER)
-#Signupbutton = customtkinter.CTkButton(master=frame, width=75, height=75, text="Signup", corner_radius=6)
-#Signupbutton.place(relx=0.65, y=175, anchor=tk.CENTER)
-# Bbutton = tk.Button(master=serverFrame, width=6, height=1, text="B", font=("Century Gothic", 20), bg="#FF5757",fg="white")
-# Bbutton.place(relx=0.5, y=175, anchor=tk.CENTER)
-# Cbutton = tk.Button(master=serverFrame, width=6, height=1, text="C", font=("Century Gothic", 20), bg="#FF914D",fg="white")
-# Cbutton.place(relx=0.80, y=175, anchor=tk.CENTER)
-
 
 frames['authentication'] = serverFrame
 
@@ -135,9 +236,10 @@ Authbutton.place(relx=0.2, y=250, anchor=tk.CENTER)
 
 frames['showAuth'] = showAuthentication
 
+#Logging into the system, in this particular case, the server only has one account assumed to be known by all humans accessing
+#The password and username is stored in cleartext in shadow.txt for ease of understanding 
 def authLogin():
     #We are simulating the sender side
-    # print(sN)
     username = EmailBox.get()
     password = PWBox.get()
     shaOb = hashlib.sha256()
@@ -152,16 +254,13 @@ def authLogin():
         use,passW = file.readlines()[0].strip().split(',')
         use = decryptor.decrypt(sD,sN,use)
         passW = decryptor.decrypt(sD,sN,passW)
-        # print(f'{use},{passW}')
     with open('shadow.txt','r') as file:
         realUse,realPass = file.readlines()[0].strip().split(',')
-        
-        if realUse == use:
+        if realUse == use: 
             shaOb = hashlib.sha256()
             shaOb.update(realPass.encode('utf-8'))
             realPass = shaOb.hexdigest()
-            # print(f'{realUse},{realPass}')
-            if realPass == passW:
+            if realPass == passW:  #If the hashed version of the password obtained by shadow.txt matches the hashed password from the user
                 changeFrame('frame2')
                 
     
@@ -182,105 +281,6 @@ Loginbutton = tk.Button(master=login, width=15, height=5, text="Login", font=("C
 Loginbutton.place(x=50, y=225, anchor=tk.CENTER)
 
 frames['login'] = login
-
-
-
-def SendEmail():
-    frame2.place_forget()
-    login.place_forget()
-    showAuthentication.place_forget()
-    serverFrame.place_forget()
-    LoginText.place_forget()
-    frame.place_forget()
-    SendMailframe.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-
-def back_to_main_frame():
-    SendMailframe.place_forget()
-    ViewEmailframe.place_forget()
-    frame2.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-
-def ViewEmail():
-    frame2.place_forget()
-    frame2.place_forget()
-    login.place_forget()
-    showAuthentication.place_forget()
-    serverFrame.place_forget()
-    LoginText.place_forget()
-    frame.place_forget()
-    getMail()
-    ViewEmailframe.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-
-def sendMail():
-    to = Toentry.get()
-    body = entry44.get("1.0", tk.END)
-    print(body)
-    # print(f"{to} | {body}")
-    sssk = rd.randint(1,100000000)
-    ssskByte = sssk.to_bytes(16, byteorder='big')
-    ssskString = f"{sssk}"
-
-    for i in keys:
-        i = i.strip().split(',')
-        if i[0] == f'{to}':
-            receiverE = int(i[1])
-            receiverN = int(i[2])
-
-    shaOb = hashlib.sha256()
-    shaOb.update(body.encode('utf-8'))
-    digiSig = shaOb.hexdigest()
-    print(digiSig)
-    digiSig = encryptor.encrypt(sD,sN,digiSig)
-
-    bodyEncrypted = symmetrickeyencrypt.encrypt_message(body,ssskByte)
-
-    sssKEncrypted = encryptor.encrypt(receiverE,receiverN,ssskString)
-
-    if to in ['A','B','C']:
-        with open(f'{to}_mailbox.txt','a') as file:
-            file.write(f'{Client},{sssKEncrypted},{digiSig},{bodyEncrypted},{datetime.now().strftime("%Y-%m-%d")}\n')
-    SendMailframe.place_forget()
-    ViewEmailframe.place_forget()
-    frame2.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-
-
-mails = []
-def getMail():
-    global mails
-    with open(f'{Client}_mailbox.txt','r') as file:
-        for i in file.readlines():
-            i = i.strip().split(',')
-            print(i)
-            sender, sssKEncrypted, digiSig, bodyEncrypted, time = i
-            sssk = decryptor.decrypt(myD,myN,sssKEncrypted)
-            body = symmetrickeyencrypt.decrypt_message(bodyEncrypted,int(sssk).to_bytes(16, byteorder='big'))
-            for i in keys:
-                i = i.strip().split(',')
-                if i[0] == f'{sender}':
-                    senderE = int(i[1])
-                    senderN = int(i[2])
-            digiSig = decryptor.decrypt(sE,sN,digiSig)
-            print(digiSig)
-            shaOb = hashlib.sha256()
-            shaOb.update(body.encode('utf-8'))
-            comparator = shaOb.hexdigest()
-            print(comparator)
-            if digiSig == comparator:
-                print('Match')
-                mails.append(f"{sender}\n{body}\n{time}\n----------\n")
-def showMails():
-    frame2.place_forget()
-    login.place_forget()
-    showAuthentication.place_forget()
-    serverFrame.place_forget()
-    LoginText.place_forget()
-    frame.place_forget()
-    getMail()
-    text_box.config(state=tk.NORMAL)  
-    text_box.delete(1.0, tk.END)
-    for mail in mails:
-        text_box.insert(tk.END, mail)
-    text_box.config(state=tk.DISABLED) 
-    ViewEmailframe.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 frame2 = customtkinter.CTkFrame(master=app, width=320, height=360, corner_radius=15)
 frame2.place_forget()
--- decryptor.py
+++ decryptor.py
@@ -1,5 +1,6 @@
 from math import log2,floor,ceil
-from utility import OneAndZeroes,decimalToBinary,effModuloExp,strToBin,detectBin,binaryToString,deciToBin
+from utility import effModuloExp,binaryToString,deciToBin
+
 def removePadding(m):
     index = 0
     if m[-1] == '1':
@@ -9,6 +10,7 @@ def removePadding(m):
             index = -i
             break
     return m[:index]
+
 def decrypt(d,n,m):
     blockSize = ceil(log2(n))
     newBlockSize = floor(log2(n))
@@ -16,27 +18,17 @@ def decrypt(d,n,m):
     partitionedMsg = []
     for i in range(0,len(binMsg),blockSize):
         partitionedMsg.append(binMsg[i:i+blockSize])
-    
     decryptedMsg = []
     for i in partitionedMsg:
         decrypted = effModuloExp(i,d,n)
         decryptedToBin = deciToBin(decrypted,newBlockSize)
         
         decryptedMsg.append(decryptedToBin)
-    # print(decryptedMsg)
     decryptedMsg[-1] = removePadding(decryptedMsg[-1])
     decryptedString = ""
     for i in decryptedMsg:
         decryptedString += i
-    # print(decryptedString)
     return binaryToString(decryptedString)
 
-
-
-# 01110101110011010111111000000100
-#1011000110101011
-# print(decrypt(53,143,"011110010000011001001001"))
-# print(decrypt(53,143,"011011000100000001000011000001010110001100010001"))
-# print(decrypt(7571,45359,"01110101110011010111111000000100"))
 
 
